ade18f8d7ce6a3a5247d99d38d979a0cba965014
Valentina Iancu <>
2018-11-03 16:55:53 +0200

    setup project in xcode added gitignore

---
+++ ReviewsAssignment/ReviewsAssignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ReviewsAssignment
+//
+//  Created by Valentina Iancu on 03/11/2018.
+//  Copyright © 2018 Valentina Iancu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
