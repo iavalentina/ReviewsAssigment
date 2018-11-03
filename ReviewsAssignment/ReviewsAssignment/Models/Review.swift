0326dcdd800fcc447bb251db237f2015f7848a70
Valentina Iancu <>
2018-11-03 22:38:25 +0200

    partial implementation for post method

--- ReviewsAssignment/ReviewsAssignment/Models/Review.swift
+++ ReviewsAssignment/ReviewsAssignment/Models/Review.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-class Review {
+struct Review {
     var score: Int = 0
     var comment: String?
     var userName: String?
--- ReviewsAssignment/ReviewsAssignment/Networking/Networking.swift
+++ ReviewsAssignment/ReviewsAssignment/Networking/Networking.swift
@@ -12,35 +12,60 @@ import Alamofire
 enum URLs {
     static let companyUrl = "https://api.hitta.se/search/v7/app/company/ctyfiintu"
     static let sandbox = "https://test.hitta.se/reviews/v1/company" // POST - save reviews to Hitta.se sandbox environment
+    /*
+     POST
+     https://test.hitta.se/reviews/v1/company
+     Content-Type: application/x-www-form-urlencoded; charset=UTF-8
+     
+     Headers
+     * X-HITTA-DEVICE-NAME: MOBILE_WEB
+     * X-HITTA-SHARED-IDENTIFIER: 15188693697264027
+     */
 }
 
 public typealias CompanyCompletionBlock = ((_ company: Company?, _ error: Error?) -> Void)
+public typealias ServiceCompletionBlock = ((_ success: Bool, _ error: Error?) -> Void)
 
 public struct Networking {
     
-    var headers: HTTPHeaders = [
-        "Content-Type": "application/json"
-    ]
+  let contentTypeHeader = ["Content-Type": "application/x-www-form-urlencoded"]
+    /*
+     * X-HITTA-DEVICE-NAME: MOBILE_WEB
+     * X-HITTA-SHARED-IDENTIFIER: 15188693697264027
+     */
     
-    func fetchCompanyDetails(_ completion: @escaping CompanyCompletionBlock){
-        var serviceError: Error? = nil
+    func fetchCompanyDetails(_ completion: @escaping CompanyCompletionBlock) {
         Alamofire.request(URLs.companyUrl,
                           method: .get,
                           encoding: URLEncoding.default)
+            .validate()
             .responseJSON { response in
-                let result = response.result
-                if result.isSuccess, let resultValue = result.value {
-                    var jsonResponse = resultValue as? [String: AnyObject]
+                switch response.result {
+                case .success:
+                    var jsonResponse = response.result.value as? [String: AnyObject]
                     if  let result = jsonResponse?["result"], let companiesJson = result["companies"] as? [String: AnyObject],
                         let companyJson =  companiesJson["company"] as? [[String: AnyObject]] {
                         let company = Company(json: companyJson[0])
                         completion(company, nil)
                     }
-                } else{
-                    serviceError = response.error
-                    print("Error request: \(String(describing: response.error))")
+                case .failure(let error):
+                    completion(nil, error)
                 }
         }
-        completion(nil, serviceError)
+    }
+    
+    // Save the user review with parameters:
+    // score, user name,
+    func saveReview(parameters: [String: AnyObject]? = nil,
+                    completion: @escaping ServiceCompletionBlock) {
+        Alamofire.request(URLs.sandbox,
+                          method: .post,
+                          parameters: parameters,
+                          encoding: JSONEncoding.default,
+                          headers: contentTypeHeader)
+            .validate()
+            .responseJSON { response in
+               completion(response.result.isSuccess, response.result.error)
+        }
     }
 }
