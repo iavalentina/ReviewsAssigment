d898c03fd0dea534448d5290ba4390c107f2fa12
Valentina Iancu <>
2018-11-04 13:44:45 +0200

    add more rules in gitignore add table view in ReviewsViewController

--- ReviewsAssignment/ReviewsAssignment/ViewControllers/ReviewsViewController.swift
+++ ReviewsAssignment/ReviewsAssignment/ViewControllers/ReviewsViewController.swift
@@ -11,12 +11,29 @@ import Alamofire
 
 class ReviewsViewController: UIViewController {
     
+    @IBOutlet var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         Networking().fetchCompanyDetails { (company, error) in
             guard let company = company else { return}
             print("company =\(company.name)")
         }
+    }
+}
+
+extension ReviewsViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
 }
 
